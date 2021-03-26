11c08cfc1f2628480b5c70a3c32fc495b6f7f391
Alexander Munz <alex.munz@yandex.ru>
2021-03-26 23:00:55 +0300

    Added UserAdmin admin class

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from profiles.user import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    def get_fieldsets(self, request, obj=None):
+        return (
+            (None, {'fields': ('profile', )}),
+            *super().get_fieldsets(request, obj),
+        )
