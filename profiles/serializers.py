4657c000bf9cb818a80d24c47c4d7d7c31b958bb
Alexander Munz <alex.munz@yandex.ru>
2021-03-27 00:17:26 +0300

    Added UserSerializer

---
+++ profiles/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from profiles.user import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+    def validate(self, validated_data):
+        if validated_data['password1'] != validated_data['password2']:
+            raise ValidationError('Пароли не совпадают')
+
+        return validated_data
+
+    def create(self, validated_data):
+        instance = self.Meta.model(username=validated_data['username'])
+        instance.set_password(validated_data['password1'])
+        instance.save()
+
+        return instance
