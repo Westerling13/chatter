aa85768d37a6252a369c3a4efb92bbe67a44d55e
Alexander Munz <alex.munz@yandex.ru>
2021-03-27 00:18:02 +0300

    Added url for UserCreateAPIView

--- project_root/urls.py
+++ project_root/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from profiles.views import UserCreateAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/', UserCreateAPIView.as_view(), name='user_create'),
 ]
