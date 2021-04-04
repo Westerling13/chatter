0358089a1376a934e2154c2379abf04a93d7ae3b
Alexander Munz <alex.munz@yandex.ru>
2021-04-04 14:25:00 +0300

    Allowed blank value for Profile.avatar field

--- profiles/models.py
+++ profiles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Profile(models.Model):
-    avatar = models.ImageField('Аватар профиля', upload_to='avatars')
+    avatar = models.ImageField('Аватар профиля', upload_to='avatars', blank=True)
 
     class Meta:
         verbose_name = 'Профиль'
