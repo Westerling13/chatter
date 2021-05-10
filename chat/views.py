f42b408513da871bfff66a914e7b659130742a3f
Alexander Munz <alex.munz@yandex.ru>
2021-05-10 21:04:49 +0300

    Added ChatMemberListCreateView

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 
 from chat.models import Chat, Message
-from chat.serializers import ChatSerializer, MessageSerializer, ChatDetailSerializer
+from chat.serializers import ChatSerializer, MessageSerializer, ChatDetailSerializer, ChatMemberSerializer
 
 
 class ChatListCreateView(generics.ListCreateAPIView):
@@ -21,6 +23,21 @@ class ChatDetailView(generics.RetrieveAPIView):
 
     def get_queryset(self):
         return Chat.objects.filter(chatmember__user=self.request.user)
+
+
+class ChatMemberListCreateView(generics.GenericAPIView):
+    serializer_class = ChatSerializer
+    lookup_url_kwarg = 'chat_id'
+    queryset = Chat.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        chat = self.get_object()
+        if chat.members.filter(id=request.user.id).exists():
+            raise ValidationError('Вы уже присоединились к этому чату.')
+
+        chat.add_member(self.request.user)
+        chat_member_serializer = ChatMemberSerializer(chat.chatmember_set.all(), many=True)
+        return Response(chat_member_serializer.data, status=status.HTTP_201_CREATED)
 
 
 class MessageListCreateView(generics.ListCreateAPIView):
