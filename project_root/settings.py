3e652b09a0914aff7b2ce783c55d6abdd0240df6
Alexander Munz <alex.munz@yandex.ru>
2021-05-16 12:28:48 +0300

    Added env variables for channels host config

--- project_root/settings.py
+++ project_root/settings.py
@@ -83,7 +83,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
+            'hosts': [(env.str('CHANNELS_HOST_IP'), env.int('CHANNELS_HOST_PORT'))],
         },
     },
 }
