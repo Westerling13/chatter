8cc240f635e49653bf47584ee9d1a7ee0510a086
Alexander Munz <alex.munz@yandex.ru>
2021-07-01 20:09:01 +0300

    Added jsw authorization

--- project_root/urls.py
+++ project_root/urls.py
@@ -1,25 +1,13 @@
-"""project_root URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from profiles.views import UserCheckAPIView, UserLogOutAPIView, UserLogInAPIView, UserRegisterAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/users/', UserRegisterAPIView.as_view(), name='user_register'),
     path('api/v1/users/me/', UserCheckAPIView.as_view(), name='me'),
     path('api/v1/login/', UserLogInAPIView.as_view(), name='login'),
