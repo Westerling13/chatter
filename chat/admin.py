ed47252a8d052917983f03a40c8296e2217c85ea
Alexander Munz <alex.munz@yandex.ru>
2021-10-13 23:59:30 +0300

    Added ChatAdmin class

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from chat.models import Message
+from chat.models import Message, Chat
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     raw_id_fields = ('sender', 'chat')
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    raw_id_fields = ('initiator',)
