3c0ae7fe82d0cdfe50525b09ad171b44397e2261
Alexander Munz <alex.munz@yandex.ru>
2021-10-14 22:52:06 +0300

    Added admin class for Attachment model

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from chat.models import Message, Chat
+from chat.models import Message, Chat, Attachment
 
 
 @admin.register(Message)
@@ -11,3 +11,8 @@ class MessageAdmin(admin.ModelAdmin):
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
     raw_id_fields = ('initiator',)
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    raw_id_fields = ('message',)
